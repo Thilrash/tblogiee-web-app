c22606b9b0429e7ea2feee40f1add7fa85c1a9ee
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2025-01-08 23:04:35 +0530

    Added date input field component

--- TblogieeClient/src/app/app.routes.ts
+++ TblogieeClient/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DateInputComponent } from './shared/date-input/date-input.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'date', component: DateInputComponent },
 ];
