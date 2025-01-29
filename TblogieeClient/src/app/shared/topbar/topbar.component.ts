5e3e0172f69268e18733307f39a7647a3c321ade
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2025-01-29 21:11:48 +0530

    Added notification card design

--- TblogieeClient/src/app/shared/topbar/topbar.component.ts
+++ TblogieeClient/src/app/shared/topbar/topbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NotificationCardComponent } from '../notification-card/notification-card.component';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [NotificationCardComponent],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
 export class TopbarComponent {
   userFullName = 'John Doe';
+  showNotification = false;
+
+  toggleNotification() {
+    this.showNotification = !this.showNotification;
+  }
 }
