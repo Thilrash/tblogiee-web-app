5c5540ca75e5b65e7c49e6dcdf01229c3b35018d
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2025-01-30 19:49:36 +0530

    added default login access

--- TblogieeClient/src/app/login/login.component.ts
+++ TblogieeClient/src/app/login/login.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+
   constructor(private router: Router) {}
+
+  navigateToHome() {
+    const emailEntry = 'admin@tblogiee.com';
+    const passwordEntry = 'admin@123';
+
+    if (emailEntry === this.email.trim() && passwordEntry === this.password.trim()) {
+      this.router.navigate(['/home']);
+    }
+  }
 
   navigateToRegister() {
     this.router.navigate(['/register']);
